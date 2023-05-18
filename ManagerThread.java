f4ad935d77946ea4be67a821f66aa6e31e387a7a
AmarGollamudi <75649289+AmarGollamudi@users.noreply.github.com>
2023-05-18 13:27:38 -0700

    S

--- ManagerThread.java
+++ ManagerThread.java
@@ -5,6 +5,14 @@ import HashMap.MyHashMap;
 import java.io.*;
 
 public class ManagerThread implements Runnable {
+    private static final double GRAVITY = .1; //, , , , , 
+    private static final double JUMPHEIGHT = 5;
+    private static final double AIRRESISTANCE = .02;
+    private static final double FRICTION = .04;
+    private static final double GROUNDMOVEMENT = .2;
+    private static final double AIRMOVEMENT = .1;
+    private static final double MAXVELOCITY = 15;
+    private static final double MINXVELOCITY = .05;//MUST BE GREATER THAN FRICTION AND AIRRESISTANCE
     private Manager manager;
     private MyHashMap<Thread, ServerThread> threadList;
     private boolean running = true;
@@ -22,6 +30,7 @@ public class ManagerThread implements Runnable {
         pHeight = 50;// player height
         gameObjects = new MyHashMap<String, Pair<Vector, double[]>>();
         sendData = new MyHashMap<String, int[]>();
+        
     }
 
     // this class is the main game calculations, it will run in a loop and update
@@ -34,7 +43,7 @@ public class ManagerThread implements Runnable {
                 double[] nums = pair.getValue();
                 double pX = nums[0];
                 double pY = nums[1];
-                v.setYDirection(v.getYDirection() + .1);
+                v.setYDirection(v.getYDirection() + GRAVITY);
                 if(pY>800){//out of bounds
                     nums[1] = 50;
                     v.setYDirection(0);
@@ -42,8 +51,7 @@ public class ManagerThread implements Runnable {
                 }
                 if(nums[2]==1){//up
                     if(nums[10]==1){
-                        System.out.println("Jumping");
-                        v.setYDirection(v.getYDirection()-5);
+                        v.setYDirection(v.getYDirection()-JUMPHEIGHT);
                     }
                 }
                 if(nums[3]==1){//down
@@ -51,15 +59,15 @@ public class ManagerThread implements Runnable {
                 }
                 if(nums[4]==1){//left
                     if(nums[10]==1){
-                        v.setXDirection(v.getXDirection()-.2);
+                        v.setXDirection(v.getXDirection()-GROUNDMOVEMENT);
                     }
-                    v.setXDirection(v.getXDirection()-.08);
+                    v.setXDirection(v.getXDirection()-AIRMOVEMENT);
                 }
                 if(nums[5]==1){//right
                     if(nums[10]==1){
-                        v.setXDirection(v.getXDirection()+.2);
+                        v.setXDirection(v.getXDirection()+GROUNDMOVEMENT);
                     }
-                    v.setXDirection(v.getXDirection()+.08);                
+                    v.setXDirection(v.getXDirection()+AIRMOVEMENT);                
                 }
                 if(nums[6]==1){//dash
 
@@ -67,24 +75,35 @@ public class ManagerThread implements Runnable {
                 if(nums[7]==1){//fire
                 }
                 if(v.getXDirection()!=0){
-                    if(v.getXDirection()>.1){
-                        v.setXDirection(v.getXDirection()-.05);
-                    } else if(v.getXDirection()<-.1){
-                        v.setXDirection(v.getXDirection()+.05);
+                    if(v.getXDirection()>MINXVELOCITY){
+                        if(nums[10]==0){
+                            v.setXDirection(v.getXDirection()-AIRRESISTANCE);
+                        }
+                        else{
+                            v.setXDirection(v.getXDirection()-FRICTION);
+
+                        }
+                    } else if(v.getXDirection()<-MINXVELOCITY){
+                        if(nums[10]==0){
+                            v.setXDirection(v.getXDirection()+AIRRESISTANCE);
+                        }
+                        else{
+                            v.setXDirection(v.getXDirection()+FRICTION);
+                        }
                     }
                     else{
                         v.setXDirection(0);
                     }
                     
                 }
-                if(v.getYDirection()>15){
-                    v.setYDirection(15);
+                if(v.getYDirection()>MAXVELOCITY){
+                    v.setYDirection(MAXVELOCITY);
                 }
-                if(v.getXDirection()>15){
-                    v.setXDirection(15);
+                if(v.getXDirection()>MAXVELOCITY){
+                    v.setXDirection(MAXVELOCITY);
                 }
-                if(v.getXDirection()<-15){
-                    v.setXDirection(-15);
+                if(v.getXDirection()<-MAXVELOCITY){
+                    v.setXDirection(-MAXVELOCITY);
                 }
                 for (Rectangle r : walls) {
                     // if touching side, xDirection = 0, x pos subtract or add
@@ -95,11 +114,11 @@ public class ManagerThread implements Runnable {
                     int wH = (int) r.getHeight();
                     
 
-                    if (pY < wY+wH && pY + pHeight > wY+1 && wX > pX && wX < pX + pWidth) {
+                    if (pY < wY+wH && pY + pHeight > wY+1 && pX<wX && pX+pWidth>wX) {
                         // TODO: touching left edge
                         if(v.getXDirection()>0){
                             v.setXDirection(0);
-                            nums[0] = wX;
+                            nums[0] = wX-pWidth;
                         }
                     }else if (pY < wY+wH && pY + pHeight > wY+1 &&  pX<wX+wW &&  pX + pWidth>wX+wW) {
                         // TODO: touching right edge
