3aef5011fc2116c698047e038ad27cee6c5a7a79
AmarGollamudi <75649289+AmarGollamudi@users.noreply.github.com>
2023-05-24 11:16:38 -0700

    s

--- Player.java
+++ Player.java
@@ -80,7 +80,6 @@ public class Player extends GameObjectStatus {
                 System.out.println("Angle: " + angle*180/3.14);
                 System.out.println("X multiplier: " + Math.cos(angle));
                 System.out.println("Y multiplier: " + Math.sin(angle));
-
                 super.getManagerThread().summonFireBall(pX, pY, new Vector(Math.cos(angle)*FIREBALL_SPEED+v.getXDirection(), Math.sin(angle)*FIREBALL_SPEED+v.getYDirection()));
                 fireCooldown = 100;
             }
